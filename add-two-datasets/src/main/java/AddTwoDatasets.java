ee5f7fdf454c1075122cf46d35e2e92ba486b626
Barry DeZonia <bdezonia@wisc.edu>
2012-08-27 10:52:50 -0500

    Add some OPS examples for adding Datasets

--- add-two-datasets/src/main/java/AddTwoDatasets.java
+++ add-two-datasets/src/main/java/AddTwoDatasets.java
@@ -19,12 +19,18 @@ import javax.swing.JFileChooser;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.img.Img;
 import net.imglib2.io.ImgIOException;
 import net.imglib2.meta.AxisType;
+import net.imglib2.ops.img.ImageCombiner;
+import net.imglib2.ops.operation.BinaryOperation;
+import net.imglib2.ops.operation.img.binary.ImgCombine;
+import net.imglib2.ops.operation.real.binary.RealAdd;
+import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 
-/** Adds two datasets using the ImgLib2 OPS framework. */
+/** Adds two datasets using the ImgLib2 framework. */
 public class AddTwoDatasets {
 
 	public static void main(final String... args) throws Exception {
@@ -39,14 +45,20 @@ public class AddTwoDatasets {
 		// add them together
 		final DatasetService datasetService =
 			context.getService(DatasetService.class);
-		final Dataset result = addRandomAccess(datasetService, dataset1, dataset2);
+		final Dataset result1 = addRandomAccess(datasetService, dataset1, dataset2);
+		final Dataset result2 =
+				addOpsSerial(datasetService, dataset1, dataset2, new FloatType());
+		final Dataset result3 =
+				addOpsParallel(datasetService, dataset1, dataset2, new FloatType());
 
 		// display the results
 		final DisplayService displayService =
 			context.getService(DisplayService.class);
 		displayService.createDisplay(dataset1.getName(), dataset1);
 		displayService.createDisplay(dataset2.getName(), dataset2);
-		displayService.createDisplay(dataset2.getName(), result);
+		displayService.createDisplay("Result: random access", result1);
+		displayService.createDisplay("Result: serial OPS", result2);
+		displayService.createDisplay("Result: parallel OPS", result3);
 	}
 
 	/**
@@ -57,7 +69,7 @@ public class AddTwoDatasets {
 	private static Dataset addRandomAccess(final DatasetService datasetService,
 		final Dataset d1, final Dataset d2)
 	{
-		final Dataset result = create(datasetService, d1, d2);
+		final Dataset result = create(datasetService, d1, d2, new FloatType());
 
 		// sum data into result dataset
 		final RandomAccess<? extends RealType> ra1 = d1.getImgPlus().randomAccess();
@@ -81,12 +93,39 @@ public class AddTwoDatasets {
 
 	/**
 	 * Adds two datasets using the ImgLib OPS framework. This is a very succinct
-	 * approach that does not require a loop. It is also automatically
+	 * approach that does not require a loop. This version is designed for small
+	 * processing jobs and is not automatically parallelized.
+	 */
+	@SuppressWarnings("unchecked")
+	private static
+		<U extends RealType<U>,V extends RealType<V>,W extends RealType<W> & NativeType<W>>
+	Dataset addOpsSerial(DatasetService dss, final Dataset d1, final Dataset d2, W outType) {
+		Dataset output = create(dss, d1, d2, outType);
+		Img<U> img1 = (Img<U>) d1.getImgPlus();
+		Img<V> img2 = (Img<V>) d2.getImgPlus();
+		Img<W> outputImg = (Img<W>) output.getImgPlus();
+		BinaryOperation<U,V,W> addOp = new RealAdd<U, V, W>();
+		ImgCombine<U,V,W> combiner = new ImgCombine<U,V,W>(addOp);
+		combiner.compute(img1, img2, outputImg);
+		return output;
+	}
+
+	/**
+	 * Adds two datasets using the ImgLib OPS framework. This is a very succinct
+	 * approach that does not require a loop. This version is automatically
 	 * parallelized!
 	 */
-	private static Dataset addOps(final Dataset d1, final Dataset d2) {
-		// BDZ TODO
-		return null;
+	@SuppressWarnings("unchecked")
+	private static
+		<U extends RealType<U>,V extends RealType<V>,W extends RealType<W> & NativeType<W>>
+	Dataset addOpsParallel(DatasetService dss, final Dataset d1, final Dataset d2, W outType) {
+		Dataset output = create(dss, d1, d2, outType);
+		Img<U> img1 = (Img<U>) d1.getImgPlus();
+		Img<V> img2 = (Img<V>) d2.getImgPlus();
+		Img<W> outputImg = (Img<W>) output.getImgPlus();
+		BinaryOperation<U,V,W> addOp = new RealAdd<U, V, W>();
+		ImageCombiner.applyOp(addOp, img1, img2, outputImg);
+		return output;
 	}
 
 	/** Loads a dataset selected by the user from a dialog box. */
@@ -107,8 +146,9 @@ public class AddTwoDatasets {
 	 * Creates a dataset with bounds constrained by the minimum of the two input
 	 * datasets.
 	 */
-	private static Dataset create(final DatasetService datasetService,
-		final Dataset d1, final Dataset d2)
+	private static <T extends RealType<T> & NativeType<T>>
+	Dataset create(final DatasetService datasetService,
+		final Dataset d1, final Dataset d2, final T type)
 	{
 		final int dimCount = Math.min(d1.numDimensions(), d2.numDimensions());
 		final long[] dims = new long[dimCount];
@@ -117,7 +157,7 @@ public class AddTwoDatasets {
 			dims[i] = Math.min(d1.dimension(i), d2.dimension(i));
 			axes[i] = d1.numDimensions() > i ? d1.axis(i) : d2.axis(i);
 		}
-		return datasetService.create(new FloatType(), dims, "result", axes);
+		return datasetService.create(type, dims, "result", axes);
 	}
 
 }
