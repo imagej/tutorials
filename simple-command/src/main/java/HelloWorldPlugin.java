149292d17a012b93867736871f5df70e4a9d2e4b
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-06-24 10:28:11 -0500

    Add a simple 'Hello, World' plugin
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ simple-command/src/main/java/HelloWorldPlugin.java
@@ -0,0 +1,16 @@
+import imagej.command.Command;
+import imagej.ui.UIService;
+
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type=Command.class)
+public class HelloWorldPlugin implements Command {
+    @Parameter
+    private UIService uiService;
+
+    @Override
+    public void run() {
+            uiService.showDialog("Hello, World!");
+    }
+}
