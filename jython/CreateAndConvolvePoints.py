40780395db7fa3e6a2c6b8e770305a6355f7593a
Brian Northan <bnorthan@gmail.com>
2015-08-19 09:26:40 -0400

    Create and Convolve points example

---
+++ jython/CreateAndConvolvePoints.py
@@ -0,0 +1,39 @@
+# @DisplayService display
+# @OpService ops
+# @int(value=128) xSize
+# @int(value=128) ySize
+
+from jarray import array
+from net.imglib2 import Point;
+
+from net.imglib2.algorithm.region.hypersphere import HyperSphere;
+
+# create an empty image 
+phantom=ops.create().img(array([xSize, ySize], 'l'))
+
+# use the randomAccess interface to place points in the image
+randomAccess= phantom.randomAccess();
+randomAccess.setPosition(array([xSize/2, ySize/2], 'l'));
+randomAccess.get().setReal(255.0);	
+
+randomAccess= phantom.randomAccess();
+randomAccess.setPosition(array([xSize/4, ySize/4], 'l'));
+randomAccess.get().setReal(255.0);
+
+location = Point(phantom.numDimensions())
+location.setPosition(array([3*xSize/4, 3*ySize/4], 'l'));
+
+hyperSphere = HyperSphere(phantom, location, 5);
+		
+for value in hyperSphere:
+	value.setReal(16);
+
+display.createDisplay("phantom", phantom)
+
+# create psf using the gaussian kernel op (alternatively PSF could be an input to the script)
+psf=ops.create().kernelGauss(array([10, 10], 'd'));
+
+# convolve psf with phantom
+convolved=ops.filter().convolve(phantom, psf);
+display.createDisplay("convolved", convolved)
+	
