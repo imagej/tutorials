f8af63a6826e7bb7cbc5d2efda22761a0419b4e5
Jan Eglinger <jan.eglinger@gmail.com>
2015-08-27 13:05:36 +0200

    CreateAndConvolvePoints: go 3D

--- jython/CreateAndConvolvePoints.py
+++ jython/CreateAndConvolvePoints.py
@@ -2,24 +2,25 @@
 # @OpService ops
 # @int(value=128) xSize
 # @int(value=128) ySize
+# @int(value=128) zSize
 
 from net.imglib2 import Point
 
 from net.imglib2.algorithm.region.hypersphere import HyperSphere
 
 # create an empty image 
-phantom=ops.create().img([xSize, ySize])
+phantom=ops.create().img([xSize, ySize, zSize])
 
 # use the randomAccess interface to place points in the image
-randomAccess= phantom.randomAccess();
-randomAccess.setPosition([xSize/2, ySize/2])
+randomAccess= phantom.randomAccess()
+randomAccess.setPosition([xSize/2, ySize/2, zSize/2])
 randomAccess.get().setReal(255.0)
 
-randomAccess.setPosition([xSize/4, ySize/4])
+randomAccess.setPosition([xSize/4, ySize/4, zSize/4])
 randomAccess.get().setReal(255.0)
 
 location = Point(phantom.numDimensions())
-location.setPosition([3*xSize/4, 3*ySize/4])
+location.setPosition([3*xSize/4, 3*ySize/4, 3*zSize/4])
 
 hyperSphere = HyperSphere(phantom, location, 5)
 		
@@ -29,7 +30,7 @@ for value in hyperSphere:
 display.createDisplay("phantom", phantom)
 
 # create psf using the gaussian kernel op (alternatively PSF could be an input to the script)
-psf=ops.create().kernelGauss([10, 10])
+psf=ops.create().kernelGauss([5, 5, 5])
 
 # convolve psf with phantom
 convolved=ops.filter().convolve(phantom, psf)
