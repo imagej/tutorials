be95760a5349c9a1f3abdc18fc0aa919df4e9411
Jan Eglinger <jan.eglinger@gmail.com>
2015-08-27 12:59:38 +0200

    Consistently use lists instead of jarray.array

--- jython/CreateAndConvolvePoints.py
+++ jython/CreateAndConvolvePoints.py
@@ -3,7 +3,6 @@
 # @int(value=128) xSize
 # @int(value=128) ySize
 
-from jarray import array
 from net.imglib2 import Point
 
 from net.imglib2.algorithm.region.hypersphere import HyperSphere
@@ -13,14 +12,14 @@ phantom=ops.create().img([xSize, ySize])
 
 # use the randomAccess interface to place points in the image
 randomAccess= phantom.randomAccess();
-randomAccess.setPosition(array([xSize/2, ySize/2], 'l'))
+randomAccess.setPosition([xSize/2, ySize/2])
 randomAccess.get().setReal(255.0)
 
-randomAccess.setPosition(array([xSize/4, ySize/4], 'l'))
+randomAccess.setPosition([xSize/4, ySize/4])
 randomAccess.get().setReal(255.0)
 
 location = Point(phantom.numDimensions())
-location.setPosition(array([3*xSize/4, 3*ySize/4], 'l'))
+location.setPosition([3*xSize/4, 3*ySize/4])
 
 hyperSphere = HyperSphere(phantom, location, 5)
 		
@@ -30,7 +29,7 @@ for value in hyperSphere:
 display.createDisplay("phantom", phantom)
 
 # create psf using the gaussian kernel op (alternatively PSF could be an input to the script)
-psf=ops.create().kernelGauss(array([10, 10], 'd'))
+psf=ops.create().kernelGauss([10, 10])
 
 # convolve psf with phantom
 convolved=ops.filter().convolve(phantom, psf)
