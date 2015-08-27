20be70f5f21895f5041e3ac1e520923a70cdd369
Brian Northan <bnorthan@gmail.com>
2015-08-26 22:34:18 -0400

    Cleanup OpsThresholdIJ1Analyze.py

--- jython/OpsThresholdIJ1Analyze.py
+++ jython/OpsThresholdIJ1Analyze.py
@@ -1,26 +1,22 @@
-# @DatasetService data
 # @DisplayService display
 # @OpService ops
 # @net.imagej.Dataset inputData
 
-from net.imglib2.meta import ImgPlus
 from net.imglib2.img.display.imagej import ImageJFunctions
-
-from jarray import array
 from ij import IJ
 
 # create a log kernel
-logKernel=ops.create().kernelLog(inputData.numDimensions(), 1.0);
+logKernel=ops.create().kernelLog(inputData.numDimensions(), 3.0);
 
 # convolve with log kernel
 logFiltered=ops.filter().convolve(inputData, logKernel);
 
 # display log filter result
-display.createDisplay("log", ImgPlus(logFiltered));
+display.createDisplay("log", logFiltered);
 
 # otsu threshold and display
 thresholded = ops.threshold().otsu(logFiltered)
-display.createDisplay("thresholded", ImgPlus(thresholded));
+display.createDisplay("thresholded", thresholded);
 
 # convert to imagej1 imageplus so we can run analyze particles
 impThresholded=ImageJFunctions.wrap(thresholded, "wrapped")
