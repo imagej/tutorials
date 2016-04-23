576f373c5ea2001d12d9ce902fb58dc5d64b3a3c
hesaid <hesaid@benjamin.su>
2016-04-23 20:44:33 +0200

    Use more annotations as http://imagej.net/Writing_plugins suggests

--- add-two-datasets/src/main/java/AddTwoDatasets.java
+++ add-two-datasets/src/main/java/AddTwoDatasets.java
@@ -23,137 +23,179 @@ import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 
-import org.scijava.widget.FileWidget;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.command.Command;
+import org.scijava.command.Previewable;
+import org.scijava.log.LogService;
+import org.scijava.app.StatusService;
+import org.scijava.ItemIO;
+import org.scijava.ItemVisibility;
+import net.imagej.DatasetService;
+import io.scif.services.DatasetIOService;
 
-/** Adds two datasets using the ImgLib2 framework. */
-public class AddTwoDatasets {
+/**
+ * Adds two datasets using the ImgLib2 framework.
+ */
+@Plugin(type = Command.class, headless = true,
+        menuPath = "Tutorials>Add Two Datasets")
+public class AddTwoDatasets implements Command, Previewable {
 
-	public static void main(final String... args) throws Exception {
-		// create the ImageJ application context with all available services
-		final ImageJ ij = new ImageJ();
+    @Parameter
+    private LogService log;
 
-		ij.ui().showUI();
+    @Parameter
+    private StatusService statusService;
 
-		// load two datasets
-		final Dataset dataset1 = load(ij);
-		final Dataset dataset2 = load(ij);
+    @Parameter
+    private DatasetService datasetService;
+    
+    @Parameter
+    private DatasetIOService datasetIOService;
 
-		if (dataset1.numDimensions() != dataset2.numDimensions()) {
-			ij.ui().showDialog(
-				"Input datasets must have the same number of dimensions.");
-			return;
-		}
+    @Parameter(visibility = ItemVisibility.MESSAGE)
+    private final String header = "This demonstration adds two datasets";
 
-		// add them together
-		final Dataset result1 = addRandomAccess(ij, dataset1, dataset2);
-		final Dataset result2 =
-			addOpsSerial(ij, dataset1, dataset2, new FloatType());
-		final Dataset result3 =
-			addOpsParallel(ij, dataset1, dataset2, new FloatType());
+    @Parameter(label = "dataset 1")
+    private File file1;
 
-		// display the results
-		ij.display().createDisplay(dataset1.getName(), dataset1);
-		ij.display().createDisplay(dataset2.getName(), dataset2);
-		ij.display().createDisplay("Result: random access", result1);
-		ij.display().createDisplay("Result: serial OPS", result2);
-		ij.display().createDisplay("Result: parallel OPS", result3);
-	}
+    @Parameter(label = "dataset 2")
+    private File file2;
 
-	/**
-	 * Adds two datasets using a loop with an ImgLib cursor. This is a very
-	 * powerful approach but requires a verbose loop.
-	 */
-	@SuppressWarnings({ "rawtypes" })
-	private static Dataset addRandomAccess(final ImageJ ij, final Dataset d1,
-		final Dataset d2)
-	{
-		final Dataset result = create(ij, d1, d2, new FloatType());
+    @Parameter(label = "Result 1", type = ItemIO.OUTPUT)
+    private Dataset result1;
+    
+    @Parameter(label = "Result 2", type = ItemIO.OUTPUT)
+    private Dataset result2;
+        
+    @Parameter(label = "Result 3", type = ItemIO.OUTPUT)
+    private Dataset result3;
 
-		// sum data into result dataset
-		final RandomAccess<? extends RealType> ra1 =
-			d1.getImgPlus().randomAccess();
-		final RandomAccess<? extends RealType> ra2 =
-			d2.getImgPlus().randomAccess();
-		final Cursor<? extends RealType> cursor =
-			result.getImgPlus().localizingCursor();
-		final long[] pos1 = new long[d1.numDimensions()];
-		final long[] pos2 = new long[d2.numDimensions()];
-		while (cursor.hasNext()) {
-			cursor.fwd();
-			cursor.localize(pos1);
-			cursor.localize(pos2);
-			ra1.setPosition(pos1);
-			ra2.setPosition(pos2);
-			final double sum = ra1.get().getRealDouble() + ra2.get().getRealDouble();
-			cursor.get().setReal(sum);
-		}
+    public static void main(final String... args) throws Exception {
+        // create the ImageJ application context with all available services
+        final ImageJ ij = net.imagej.Main.launch(args);
 
-		return result;
-	}
+        ij.command().run(AddTwoDatasets.class, true);
+    }
 
-	/**
-	 * Adds two datasets using the ImgLib OPS framework. This is a very succinct
-	 * approach that does not require a loop. This version is designed for small
-	 * processing jobs and is not automatically parallelized.
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private static <T extends RealType<T> & NativeType<T>> Dataset addOpsSerial(
-		final ImageJ ij, final Dataset d1, final Dataset d2, final T outType)
-	{
-		final Dataset output = create(ij, d1, d2, outType);
-		final Img img1 = d1.getImgPlus();
-		final Img img2 = d2.getImgPlus();
-		final Img outputImg = output.getImgPlus();
-		final BinaryOperation addOp = new RealAdd();
-		final ImgCombine combiner = new ImgCombine(addOp);
-		combiner.compute(img1, img2, outputImg);
-		return output;
-	}
+    @Override
+    public void run() {
+        // add them together
+        Dataset dataset1, dataset2;
+        try {
+            dataset1 = datasetIOService.open(file1.getAbsolutePath());
+        }
+        catch (IOException e) {
+            log.error(e);
+            return;
+        }
+        try {
+            dataset2 = datasetIOService.open(file2.getAbsolutePath());
+        }
+        catch (IOException e) {
+            log.error(e);
+            return;
+        }
+        if (dataset1.numDimensions() != dataset2.numDimensions()) {
+            log.error("Input datasets must have the same number of dimensions.");
+            return;
+        }
+        result1 = addRandomAccess(dataset1, dataset2);
+        result2 = addOpsSerial(dataset1, dataset2, new FloatType());
+	result3 = addOpsParallel(dataset1, dataset2, new FloatType());
+    }
+    
+    @Override
+    public void cancel() {
+        log.info("Cancelled");
+    }
+    
+    @Override
+    public void preview() {
+        log.info("previews AddTwoDatasets");
+        statusService.showStatus(header);
+    }
+        
+    /**
+     * Adds two datasets using a loop with an ImgLib cursor. This is a very
+     * powerful approach but requires a verbose loop.
+     */
+    @SuppressWarnings({"rawtypes"})
+    private Dataset addRandomAccess(final Dataset d1,
+            final Dataset d2) {
+        final Dataset result = create(d1, d2, new FloatType());
 
-	/**
-	 * Adds two datasets using the ImgLib OPS framework. This is a very succinct
-	 * approach that does not require a loop. This version is automatically
-	 * parallelized!
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private static <T extends RealType<T> & NativeType<T>> Dataset
-		addOpsParallel(final ImageJ ij, final Dataset d1, final Dataset d2,
-			final T outType)
-	{
-		final Dataset output = create(ij, d1, d2, outType);
-		final Img img1 = d1.getImgPlus();
-		final Img img2 = d2.getImgPlus();
-		final Img outputImg = output.getImgPlus();
-		final BinaryOperation addOp = new RealAdd();
-		ImageCombiner.applyOp(addOp, img1, img2, outputImg);
-		return output;
-	}
+        // sum data into result dataset
+        final RandomAccess<? extends RealType> ra1
+                = d1.getImgPlus().randomAccess();
+        final RandomAccess<? extends RealType> ra2
+                = d2.getImgPlus().randomAccess();
+        final Cursor<? extends RealType> cursor
+                = result.getImgPlus().localizingCursor();
+        final long[] pos1 = new long[d1.numDimensions()];
+        final long[] pos2 = new long[d2.numDimensions()];
+        while (cursor.hasNext()) {
+            cursor.fwd();
+            cursor.localize(pos1);
+            cursor.localize(pos2);
+            ra1.setPosition(pos1);
+            ra2.setPosition(pos2);
+            final double sum = ra1.get().getRealDouble() + ra2.get().getRealDouble();
+            cursor.get().setReal(sum);
+        }
 
-	/** Loads a dataset selected by the user from a dialog box. */
-	private static Dataset load(final ImageJ ij) throws IOException {
-		// ask the user for a file to open
-		final File file = ij.ui().chooseFile(null, FileWidget.OPEN_STYLE);
-		if (file == null) return null;
+        return result;
+    }
 
-		// load the dataset
-		return ij.dataset().open(file.getAbsolutePath());
-	}
+    /**
+     * Adds two datasets using the ImgLib OPS framework. This is a very succinct
+     * approach that does not require a loop. This version is designed for small
+     * processing jobs and is not automatically parallelized.
+     */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private <T extends RealType<T> & NativeType<T>> Dataset addOpsSerial(
+            final Dataset d1, final Dataset d2, final T outType) {
+        final Dataset output = create(d1, d2, outType);
+        final Img img1 = d1.getImgPlus();
+        final Img img2 = d2.getImgPlus();
+        final Img outputImg = output.getImgPlus();
+        final BinaryOperation addOp = new RealAdd();
+        final ImgCombine combiner = new ImgCombine(addOp);
+        combiner.compute(img1, img2, outputImg);
+        return output;
+    }
 
-	/**
-	 * Creates a dataset with bounds constrained by the minimum of the two input
-	 * datasets.
-	 */
-	private static <T extends RealType<T> & NativeType<T>> Dataset create(
-		final ImageJ ij, final Dataset d1, final Dataset d2, final T type)
-	{
-		final int dimCount = Math.min(d1.numDimensions(), d2.numDimensions());
-		final long[] dims = new long[dimCount];
-		final AxisType[] axes = new AxisType[dimCount];
-		for (int i = 0; i < dimCount; i++) {
-			dims[i] = Math.min(d1.dimension(i), d2.dimension(i));
-			axes[i] = d1.numDimensions() > i ? d1.axis(i).type() : d2.axis(i).type();
-		}
-		return ij.dataset().create(type, dims, "result", axes);
-	}
+    /**
+     * Adds two datasets using the ImgLib OPS framework. This is a very succinct
+     * approach that does not require a loop. This version is automatically
+     * parallelized!
+     */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private <T extends RealType<T> & NativeType<T>> Dataset
+            addOpsParallel(final Dataset d1, final Dataset d2,
+                    final T outType) {
+        final Dataset output = create(d1, d2, outType);
+        final Img img1 = d1.getImgPlus();
+        final Img img2 = d2.getImgPlus();
+        final Img outputImg = output.getImgPlus();
+        final BinaryOperation addOp = new RealAdd();
+        ImageCombiner.applyOp(addOp, img1, img2, outputImg);
+        return output;
+    }
+    /**
+     * Creates a dataset with bounds constrained by the minimum of the two input
+     * datasets.
+     */
+    private <T extends RealType<T> & NativeType<T>> Dataset create(
+            final Dataset d1, final Dataset d2, final T type) {
+        final int dimCount = Math.min(d1.numDimensions(), d2.numDimensions());
+        final long[] dims = new long[dimCount];
+        final AxisType[] axes = new AxisType[dimCount];
+        for (int i = 0; i < dimCount; i++) {
+            dims[i] = Math.min(d1.dimension(i), d2.dimension(i));
+            axes[i] = d1.numDimensions() > i ? d1.axis(i).type() : d2.axis(i).type();
+        }
+        return datasetService.create(type, dims, "result", axes);
+    }
 
 }
