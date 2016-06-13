6888fcf7cbe19ec84e5805f699f5146a73772047
Brian Northan <bnorthan@gmail.com>
2016-06-13 11:46:51 -0400

    Open and display images

--- neural-network/src/main/java/NeuralNetworkExample.java
+++ neural-network/src/main/java/NeuralNetworkExample.java
@@ -1,4 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+
+import net.imagej.Dataset;
+import net.imagej.ImageJ;
+import net.imglib2.type.numeric.RealType;
 
 public class NeuralNetworkExample {
+	public static <T extends RealType<T>> void main(final String... args) throws IOException {
+		ImageJ ij = net.imagej.Main.launch();
+
+		String inputName = "./resources/cnn_input.png";
+		String targetName = "./resources/cnn_target.png";
+
+		final Dataset input = ij.dataset().open(new File(inputName).getAbsolutePath());
+		final Dataset target = ij.dataset().open(new File(targetName).getAbsolutePath());
+
+		ij.ui().show(input);
+		ij.ui().show(target);
+	}
 
 }
