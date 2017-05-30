b784943ea2c73f8b73535245783524c54c5e0f14
Curtis Rueden <ctrueden@wisc.edu>
2017-05-30 10:42:13 -0500

    Fix the Travis build
    It now needs to enter the maven-projects folder before building.

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,9 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
   -a "$TRAVIS_BRANCH" = master ]
 then
+  cd maven-projects &&
   mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml"
 else
+  cd maven-projects &&
   mvn install
 fi
