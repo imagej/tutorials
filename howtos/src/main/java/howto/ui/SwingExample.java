abdc709da935f6e7ab581acc14f4ee3d20a11a44
Edward Evans <elevans2@wisc.edu>
2020-02-23 13:26:52 -0600

    Moved SwingExample from maven-projects to howtos

---
+++ howtos/src/main/java/howto/ui/SwingExample.java
@@ -0,0 +1,19 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     https://creativecommons.org/publicdomain/zero/1.0/
+ */
+package howto.ui;
+
+import net.imagej.ImageJ;
+
+public class SwingExample {
+
+	public static void main(final String[] args) {
+		final ImageJ ij = new ImageJ();
+		ij.launch(args);
+	}
+
+}
