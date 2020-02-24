583d63fe491ba71970fcf39f2f8baba7d640f44a
Edward Evans <elevans2@wisc.edu>
2020-02-24 12:16:59 -0600

    Moved from create-a-new-plugin-type to howtos

---
+++ howtos/src/main/java/howto/plugins/create/Bear.java
@@ -0,0 +1,21 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     https://creativecommons.org/publicdomain/zero/1.0/
+ */
+package howto.plugins.create;
+
+import org.scijava.plugin.Plugin;
+
+/** An animal with tremendous strength and adorable cubs. */
+@Plugin(type = Animal.class, name = "Bear")
+public class Bear implements Animal {
+
+	@Override
+	public boolean isCat() {
+		return false;
+	}
+
+}
---
+++ howtos/src/main/java/howto/plugins/create/Lion.java
@@ -0,0 +1,21 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     https://creativecommons.org/publicdomain/zero/1.0/
+ */
+package howto.plugins.create;
+
+import org.scijava.plugin.Plugin;
+
+/** An animal that rules the jungle. */
+@Plugin(type = Animal.class, name = "Lion")
+public class Lion implements Animal {
+
+	@Override
+	public boolean isCat() {
+		return true;
+	}
+
+}
